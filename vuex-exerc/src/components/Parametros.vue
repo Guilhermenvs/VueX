231d5a92e5de8291ee40d70e37b38a1c88d90660
Guilhermenvs <nevesguilherme399@gmail.com>
2022-08-04 15:06:47 -0300

    guivuex

---
+++ vuex-exerc/src/components/Parametros.vue
@@ -0,0 +1,44 @@
+<template>
+    <Painel titulo="Parametros" vermelho>
+        <div class="parametros">
+            <span>
+                <strong>Quantidade Padrao: </strong>
+                <input type="number" v-model="quantidade">
+            </span>
+            <span>
+                <strong>Preco Padrao: </strong>
+                <input type="number" v-model="preco">
+            </span>
+        </div>
+    </Painel>
+</template>
+
+<script>
+    export default {
+        computed: {
+            quantidade: {
+                get() {
+                    return this.$store.state.parametros.quantidade
+                },
+                set(valor) {
+                    this.$store.commit('setQuantidade', valor)
+                }
+            },
+            preco: {
+                get() {
+                    return this.$store.state.parametros.preco
+                },
+                set(valor) {
+                    this.$store.commit('setPreco', valor)
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    .parametros {
+        display: flex;
+        justify-content: space-around;
+    }
+</style>
